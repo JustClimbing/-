f195553e1e87d5913fe53fbef8398c9ad3667754
lijiale <2583390175@qq.com>
2024-08-02 16:47:27 +0800

    Finish feature001 Menu

--- 通讯录管理系统/通讯录管理系统.cpp
+++ 通讯录管理系统/通讯录管理系统.cpp
@@ -9,10 +9,21 @@
 ·清空联系人：清空通讯录中所有信息
 ·退出通讯录：退出当前使用的通讯录*/
 #include <iostream>
-using namespace std;
 
+using namespace std;
+void showMenu()
+{
+	cout << "1、添加联系人" << endl;
+	cout << "2、显示联系人" << endl;
+	cout << "3、删除联系人" << endl;
+	cout << "4、查找联系人" << endl;
+	cout << "5、修改联系人" << endl;
+	cout << "6、清空联系人" << endl;
+	cout << "0、退出通讯录" << endl;
+}
 int main()
 {
+	showMenu();
 	system("pause"); //按任意键继续，不会让程序一闪而过
 		return 0;
 }
