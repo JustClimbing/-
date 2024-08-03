414281721bfdb47d887fa949661f6900ac6fa2a4
lijiale <2583390175@qq.com>
2024-08-03 09:05:09 +0800

    feature002 select and exit

--- 通讯录管理系统/通讯录管理系统.cpp
+++ 通讯录管理系统/通讯录管理系统.cpp
@@ -9,21 +9,57 @@
 ·清空联系人：清空通讯录中所有信息
 ·退出通讯录：退出当前使用的通讯录*/
 #include <iostream>
-
 using namespace std;
+
+//菜单界面
 void showMenu()
 {
-	cout << "1、添加联系人" << endl;
-	cout << "2、显示联系人" << endl;
-	cout << "3、删除联系人" << endl;
-	cout << "4、查找联系人" << endl;
-	cout << "5、修改联系人" << endl;
-	cout << "6、清空联系人" << endl;
-	cout << "0、退出通讯录" << endl;
+	cout << "*************************" << endl;
+	cout << "***** 1、添加联系人 *****" << endl;
+	cout << "***** 2、显示联系人 *****"  << endl;
+	cout << "***** 3、删除联系人 *****" << endl;
+	cout << "***** 4、查找联系人 *****" << endl;
+	cout << "***** 5、修改联系人 *****" << endl;
+	cout << "***** 6、清空联系人 *****" << endl;
+	cout << "***** 0、退出通讯录 *****" << endl;
+	cout << "*************************" << endl;
 }
+
 int main()
 {
-	showMenu();
+	showMenu(); //调用菜单
+
+	int select = 0;
+	
+	//选择功能
+	while (true)//使其只有按0时才能退出
+	{
+		cin >> select;
+		switch (select)
+		{
+		case 1:// 1、添加联系人 
+			break;
+		case 2:// 2、显示联系人 
+			break;
+		case 3:// 3、删除联系人 
+			break;
+		case 4:// 4、查找联系人 
+			break;
+		case 5:// 5、修改联系人 
+			break;
+		case 6:// 6、清空联系人
+			break;
+		case 0://退出功能
+			cout << "欢迎下次使用" << endl;
+			system("pause");
+			return 0;
+			break;
+		default:
+			break;
+		}
+	}
+	
+
 	system("pause"); //按任意键继续，不会让程序一闪而过
 		return 0;
 }
